49e241f61e250bd408b53064d686a96075ca5943
Vivek Sharma <viveksupmanyu@gmail.com>
2021-09-18 11:34:32 +0530

    dd solve

--- index.php
+++ index.php
@@ -439,7 +439,6 @@
                   h-56
                   object-cover object-center
                   mb-4
-
                 " src="vivek.jpg" />
             <div class="w-full">
               <h2 class="title-font font-medium text-lg text-gray-900">
@@ -472,8 +471,7 @@
 
           </div>
         </div>
-      </div>
-      <div class="
+        <div class="
               animate__animated
               wow
               animate__heartBeat animate__delay-4s animate__slow
@@ -481,8 +479,8 @@
               lg:w-1/4
               md:w-1/2
             ">
-        <div class="h-full flex flex-col items-center text-center">
-          <img alt="team" class="
+          <div class="h-full flex flex-col items-center text-center">
+            <img alt="team" class="
                   flex-shrink-0
                   rounded-full
                   w-56
@@ -491,37 +489,36 @@
                   mb-4
 
                 " src="manvi.jpeg" />
-          <div class="w-full">
-            <h2 class="title-font font-medium text-lg text-gray-900">
-              Manvi Chauhan
-            </h2>
-            <h3 class="text-gray-500 mb-3">Database Administrator</h3>
-            <p class="mb-4">
-              Highly intelligent and talented person with kind nature and a beautiful smile.
-            </p>
-            <span class="inline-flex">
-              <a class="text-gray-500">
-                <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewbox="0 0 24 24">
-                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                </svg>
-              </a>
-              <a class="ml-2 text-gray-500">
-                <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewbox="0 0 24 24">
-                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                </svg>
-              </a>
-              <a class="ml-2 text-gray-500">
-                <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewbox="0 0 24 24">
-                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                </svg>
-              </a>
-            </span>
-          </div>
+            <div class="w-full">
+              <h2 class="title-font font-medium text-lg text-gray-900">
+                Manvi Chauhan
+              </h2>
+              <h3 class="text-gray-500 mb-3">Database Administrator</h3>
+              <p class="mb-4">
+                Highly intelligent and talented person with kind nature and a beautiful smile.
+              </p>
+              <span class="inline-flex">
+                <a class="text-gray-500">
+                  <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewbox="0 0 24 24">
+                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                  </svg>
+                </a>
+                <a class="ml-2 text-gray-500">
+                  <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewbox="0 0 24 24">
+                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                  </svg>
+                </a>
+                <a class="ml-2 text-gray-500">
+                  <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewbox="0 0 24 24">
+                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                  </svg>
+                </a>
+              </span>
+            </div>
 
+          </div>
         </div>
-      </div>
-    </div>
-    <div class="
+        <div class="
               animate__animated
               wow
               animate__heartBeat animate__delay-5s animate__slow
@@ -529,8 +526,8 @@
               lg:w-1/4
               md:w-1/2
             ">
-      <div class="h-full flex flex-col items-center text-center">
-        <img alt="team" class="
+          <div class="h-full flex flex-col items-center text-center">
+            <img alt="team" class="
                   flex-shrink-0
                   rounded-full
                   w-56
@@ -539,37 +536,36 @@
                   mb-4
 
                 " src="sikhar.jpeg" />
-        <div class="w-full">
-          <h2 class="title-font font-medium text-lg text-gray-900">
-            Shikhar Singhal
-          </h2>
-          <h3 class="text-gray-500 mb-3">Frontend Developer</h3>
-          <p class="mb-4">
-            Good person with Cold mind and simple living habit.
-          </p>
-          <span class="inline-flex">
-            <a class="text-gray-500">
-              <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewbox="0 0 24 24">
-                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-              </svg>
-            </a>
-            <a class="ml-2 text-gray-500">
-              <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewbox="0 0 24 24">
-                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-              </svg>
-            </a>
-            <a class="ml-2 text-gray-500">
-              <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewbox="0 0 24 24">
-                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-              </svg>
-            </a>
-          </span>
-        </div>
+            <div class="w-full">
+              <h2 class="title-font font-medium text-lg text-gray-900">
+                Shikhar Singhal
+              </h2>
+              <h3 class="text-gray-500 mb-3">Frontend Developer</h3>
+              <p class="mb-4">
+                Good person with Cold mind and simple living habit.
+              </p>
+              <span class="inline-flex">
+                <a class="text-gray-500">
+                  <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewbox="0 0 24 24">
+                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                  </svg>
+                </a>
+                <a class="ml-2 text-gray-500">
+                  <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewbox="0 0 24 24">
+                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                  </svg>
+                </a>
+                <a class="ml-2 text-gray-500">
+                  <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewbox="0 0 24 24">
+                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                  </svg>
+                </a>
+              </span>
+            </div>
 
-      </div>
-    </div>
-    </div>
-    <div class="
+          </div>
+        </div>
+        <div class="
               animate__animated
               wow
               animate__heartBeat animate__delay-3s animate__slow
@@ -577,8 +573,8 @@
               lg:w-1/4
               md:w-1/2
             ">
-      <div class="h-full flex flex-col items-center text-center">
-        <img alt="team" class="
+          <div class="h-full flex flex-col items-center text-center">
+            <img alt="team" class="
                   flex-shrink-0
                   rounded-full
                   w-56
@@ -587,37 +583,36 @@
                   mb-4
 
                 " src="visha.jpg" />
-        <div class="w-full">
-          <h2 class="title-font font-medium text-lg text-gray-900">
-            Vishakha Gandhi
-          </h2>
-          <h3 class="text-gray-500 mb-3">Graphics designer</h3>
-          <p class="mb-4">
-            Highly Qualified person with Humble nature and Great senseofhumour.
-          </p>
-          <span class="inline-flex">
-            <a class="text-gray-500">
-              <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewbox="0 0 24 24">
-                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-              </svg>
-            </a>
-            <a class="ml-2 text-gray-500">
-              <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewbox="0 0 24 24">
-                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-              </svg>
-            </a>
-            <a class="ml-2 text-gray-500">
-              <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewbox="0 0 24 24">
-                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-              </svg>
-            </a>
-          </span>
-        </div>
+            <div class="w-full">
+              <h2 class="title-font font-medium text-lg text-gray-900">
+                Vishakha Gandhi
+              </h2>
+              <h3 class="text-gray-500 mb-3">Graphics designer</h3>
+              <p class="mb-4">
+                Highly Qualified person with Humble nature and Great senseofhumour.
+              </p>
+              <span class="inline-flex">
+                <a class="text-gray-500">
+                  <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewbox="0 0 24 24">
+                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                  </svg>
+                </a>
+                <a class="ml-2 text-gray-500">
+                  <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewbox="0 0 24 24">
+                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                  </svg>
+                </a>
+                <a class="ml-2 text-gray-500">
+                  <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewbox="0 0 24 24">
+                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                  </svg>
+                </a>
+              </span>
+            </div>
 
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
-    </div>
     </div>
   </section>
   <section id="contact" class="
